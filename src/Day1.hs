a84bb5ad6729c44e054f5db3346843f0bdb4bf37
cwainwright <86470077+cwainwright@users.noreply.github.com>
2024-12-01 19:31:46 +0000

    Day 1 - Part Two

--- src/Day1.hs
+++ src/Day1.hs
@@ -4,16 +4,23 @@ module Day1 where
   
   main :: IO()
   main = do
+    -- Part 1
     file_contents <- readFile "data/D1.txt"
     let parsedData = parseData file_contents
-    let (list1, list2) = unzip parsedData
-    let (sList1, sList2) = (sort list1, sort list2)
-    let sortedLists = zip sList1 sList2
+    let (leftList, rightList) = unzip parsedData
+    let (sLeftList, sRightList) = (sort leftList, sort rightList)
+    let sortedLists = zip sLeftList sRightList
     let differences = map (\(x, y) -> abs (x - y)) sortedLists
-    let total = sum differences
-    putStrLn $ show total
+    let total1 = sum differences
+    putStrLn $ "Part 1 Result: " ++ show total1
+    -- Part 1 Complete
+    -- Part 2
+    let similarityScores = map (calculateSimilarity rightList) leftList
+    let total2 = sum similarityScores
+    putStrLn $ "Part 2 Result: " ++ show total2
+    -- Part 2 Complete
 
-  
+  -- Part 1 Helper Functions
   parseData :: String -> [(Int, Int)]
   parseData str = 
     case parse dataParser str of
@@ -29,3 +36,7 @@ module Day1 where
     _ <- whitespaceP
     l2 <- numberP
     return (l1, l2)
+
+  -- Part 2 Helper Functions
+  calculateSimilarity :: [Int] -> Int -> Int
+  calculateSimilarity rightList number = number * (length $ filter (\x -> x == number) rightList)
