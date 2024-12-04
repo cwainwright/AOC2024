578645503521d16e4cf2cebde03e8a009070d157
cwainwright <86470077+cwainwright@users.noreply.github.com>
2024-12-04 06:24:28 +0000

    Day 3 Solution - Completed Part One and Part Two of Solution - Update Parser to use Maybe

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,1 +1,102 @@
 module Day3 where
+
+import Control.Applicative ((<|>))
+import Parser (Parser (..), characterP, keywordP, numberP)
+
+main :: IO ()
+main = do
+  file_contents <- readFile "data/D3.txt"
+
+  -- Part One
+  let mMul = parse (parseInstances mulP) file_contents
+   in execIO mMul
+
+  -- Part Two
+  let mInst = parse (parseInstances (mulP <|> doP <|> don'tP)) file_contents
+   in execIO mInst
+
+data MMInst = Mul Int Int | Do | Don't deriving (Show)
+
+mulP :: Parser MMInst
+mulP = do
+  _ <- keywordP "mul"
+  _ <- characterP '('
+  num1 <- numberP
+  _ <- characterP ','
+  num2 <- numberP
+  _ <- characterP ')'
+  return $ Mul num1 num2
+
+doP :: Parser MMInst
+doP = keywordP "do()" >> return Do
+
+don'tP :: Parser MMInst
+don'tP = keywordP "don't()" >> return Don't
+
+parseInstances :: Parser a -> Parser [a]
+parseInstances parser = Parser $ \input1 -> do
+  case input1 of
+    [] -> (Just ([], []))
+    _ -> case parse parser input1 of
+      Just (result, remainder) -> do
+        (more, _) <- parse (parseInstances parser) remainder
+        Just (result : more, [])
+      Nothing -> do
+        let (_ : input2) = input1
+        parse (parseInstances parser) input2
+
+-- (runTotal, enable)
+type MMEnv = (Int, Bool)
+
+newtype MMState a = MMState {exec :: MMEnv -> Maybe (a, MMEnv)}
+
+instance Functor MMState where
+  fmap f (MMState s) = MMState $ \state ->
+    case s state of
+      Nothing -> Nothing
+      Just (a, newState) -> Just (f a, newState)
+
+instance Applicative MMState where
+  pure a = MMState $ \state -> Just (a, state)
+  (MMState sa) <*> (MMState sb) = MMState $ \state ->
+    case sa state of
+      Nothing -> Nothing
+      Just (a, state') -> case sb state' of
+        Nothing -> Nothing
+        Just (b, state'') -> Just (a b, state'')
+
+instance Monad MMState where
+  return = pure
+  (>>=) :: MMState a -> (a -> MMState b) -> MMState b
+  (MMState sa) >>= f = MMState $ \state ->
+    case sa state of
+      Nothing -> Nothing
+      Just (a, state') -> exec (f a) state'
+
+get :: MMState MMEnv
+get = MMState $ \state -> Just (state, state)
+
+modify :: (MMEnv -> MMEnv) -> MMState ()
+modify f = MMState $ \state -> Just ((), f state)
+
+active :: MMState Int
+active = MMState $ \(total, enable) -> Just (if enable then 1 else 0, (total, enable))
+
+execInst :: MMInst -> MMState ()
+execInst inst = case inst of
+  Do -> modify (\(total, _) -> (total, True))
+  Don't -> modify (\(total, _) -> (total, False))
+  Mul a b -> modify (\(total, enable) -> (total + (a * b * (if enable then 1 else 0)), enable))
+
+execAll :: [MMInst] -> MMState Int
+execAll instrs = do
+  mapM_ execInst instrs
+  (total, _) <- get
+  return total
+
+execIO :: Maybe ([MMInst], a) -> IO ()
+execIO Nothing = putStrLn "Parse Error"
+execIO (Just (inst, _)) = do
+  case exec (execAll inst) (0, True) of
+    Nothing -> putStrLn "Execution Error"
+    Just (total, _) -> print total
--- src/Parser.hs
+++ src/Parser.hs
@@ -1,122 +1,134 @@
-module Parser(Parser(..), errMsg, commaSeparated, semicolonSeparated, keywordP, characterP, whitespaceP, whitespaceRP, numberP, boolP, identifierP, characterSeparated, newline, parserSeparated) where
-    import Control.Applicative (Alternative(empty, (<|>)), many, some)
-    import Data.Char (isAlphaNum, isAlpha, isSpace, isDigit)
-    import Control.Monad (void)
-    
-    newtype Parser a = Parser {parse :: String -> Either String (a, String)}
+{-# LANGUAGE LambdaCase #-}
 
-    errMsg :: String -> String -> Either String a
-    errMsg err ns = Left (err ++ "'" ++ (take 20 $ takeWhile (\c -> c /= '\n') ns) ++ "...'")
+module Parser (Parser (..), commaSeparated, semicolonSeparated, keywordP, characterP, whitespaceP, whitespaceRP, numberP, boolP, identifierP, characterSeparated, newline, parserSeparated, alphaNumP) where
 
-    instance Functor Parser where
-        fmap :: (a -> b) -> Parser a -> Parser b
-        fmap f (Parser p) = Parser $ \input -> case p input of
-            Left err -> Left err
-            Right (x, rest) -> Right (f x, rest)
+import Control.Applicative (Alternative (empty, (<|>)), many, some)
+import Control.Monad (void)
+import Data.Char (isAlpha, isAlphaNum, isDigit, isSpace)
 
-    instance Applicative Parser where
-        pure :: a -> Parser a
-        pure x = Parser $ \input -> Right (x, input)
-        (<*>) :: Parser (a -> b) -> Parser a -> Parser b
-        (Parser f) <*> (Parser p) = Parser $ \input -> case f input of
-            Left err -> Left err
-            Right (g, rest) -> case p rest of
-                Left err -> Left err
-                Right (x, r) -> Right (g x, r)
+newtype Parser a = Parser {parse :: String -> Maybe (a, String)}
 
-    instance Monad Parser where
-        return :: a -> Parser a
-        return = pure
-        (>>=) :: Parser a -> (a -> Parser b) -> Parser b
-        (Parser p) >>= f = Parser $ \input -> case p input of
-            Left err -> Left err
-            Right (x, rest) -> parse (f x) rest
+instance Functor Parser where
+  fmap :: (a -> b) -> Parser a -> Parser b
+  fmap f (Parser p) = Parser $ \input -> case p input of
+    Just (x, rest) -> Just (f x, rest)
+    Nothing -> Nothing
 
-    instance Alternative Parser where
-        empty :: Parser a
-        empty = Parser $ const Left "Parse Failed"
-        (<|>) :: Parser a -> Parser a -> Parser a
-        (Parser p1) <|> (Parser p2) = Parser $ \input -> case p1 input of
-            Left _ -> p2 input
-            result -> result
+instance Applicative Parser where
+  pure :: a -> Parser a
+  pure x = Parser $ \input -> Just (x, input)
+  (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+  (Parser f) <*> (Parser p) = Parser $ \input -> case f input of
+    Nothing -> Nothing
+    Just (g, rest) -> case p rest of
+      Nothing -> Nothing
+      Just (x, r) -> Just (g x, r)
 
-    -- Generalised Parsers
-    characterSeparated :: Char -> Parser a -> Parser [a]
-    characterSeparated c parser = 
-        (do
-            first <- parser
-            rest <- many (do
-                _ <- whitespaceP
-                _ <- characterP c
-                _ <- whitespaceP
-                parser)
-            return (first : rest))
-        <|> return []
+instance Monad Parser where
+  return :: a -> Parser a
+  return = pure
+  (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+  (Parser p) >>= f = Parser $ \input -> case p input of
+    Nothing -> Nothing
+    Just (x, rest) -> parse (f x) rest
 
-    parserSeparated :: Parser b -> Parser a -> Parser [a]
-    parserSeparated separatorParser parser = 
-        (do
-            first <- parser
-            rest <- many (do
-                _ <- whitespaceP
-                _ <- separatorParser
-                _ <- whitespaceP
-                parser)
-            return (first : rest))
-        <|> return []
+instance Alternative Parser where
+  empty :: Parser a
+  empty = Parser $ const Nothing
+  (<|>) :: Parser a -> Parser a -> Parser a
+  (Parser p1) <|> (Parser p2) = Parser $ \input -> case p1 input of
+    Nothing -> p2 input
+    result -> result
 
-    commaSeparated :: Parser a -> Parser [a]
-    commaSeparated parser = characterSeparated ',' parser
+-- Generalised Parsers
+characterSeparated :: Char -> Parser a -> Parser [a]
+characterSeparated c parser =
+  ( do
+      first <- parser
+      rest <-
+        many
+          ( do
+              _ <- whitespaceP
+              _ <- characterP c
+              _ <- whitespaceP
+              parser
+          )
+      return (first : rest)
+  )
+    <|> return []
 
-    semicolonSeparated :: Parser a -> Parser [a]
-    semicolonSeparated parser = characterSeparated ';' parser
+parserSeparated :: Parser b -> Parser a -> Parser [a]
+parserSeparated separatorParser parser =
+  ( do
+      first <- parser
+      rest <-
+        many
+          ( do
+              _ <- whitespaceP
+              _ <- separatorParser
+              _ <- whitespaceP
+              parser
+          )
+      return (first : rest)
+  )
+    <|> return []
 
-    keywordP :: String -> Parser String
-    keywordP s = do
-        Parser $ \input ->
-            if s == take (length s) input
-            then Right (s, drop (length s) input)
-            else errMsg ("expected keyword '" ++ s ++ "', ") input
+commaSeparated :: Parser a -> Parser [a]
+commaSeparated parser = characterSeparated ',' parser
 
-    characterP :: Char -> Parser Char
-    characterP c = Parser $ \ns -> case ns of
-        (x:xs) | x == c -> Right (x, xs)
-        _ -> errMsg ("expected character '" ++ [c] ++ "', ") ns
+semicolonSeparated :: Parser a -> Parser [a]
+semicolonSeparated parser = characterSeparated ';' parser
 
-    spaceP :: Parser Char
-    spaceP = Parser $ \ns -> case ns of
-        (x : xs) | isSpace x -> Right (x, xs)
-        _ -> errMsg "missing: expected ' ' (space), " ns
+keywordP :: String -> Parser String
+keywordP s = do
+  Parser $ \input ->
+    if s == take (length s) input
+      then Just (s, drop (length s) input)
+      else Nothing
 
-    whitespaceP :: Parser ()
-    whitespaceP = void $ many spaceP
+characterP :: Char -> Parser Char
+characterP c = Parser $ \ns -> case ns of
+  (x : xs) | x == c -> Just (x, xs)
+  _ -> Nothing
 
-    whitespaceRP :: Parser ()
-    whitespaceRP = void $ some spaceP
+spaceP :: Parser Char
+spaceP = Parser $ \ns -> case ns of
+  (x : xs) | isSpace x -> Just (x, xs)
+  _ -> Nothing
 
-    numberP :: Parser Int
-    numberP = fmap read (some $ Parser $ \ns -> case ns of
-        (x : xs) | isDigit x -> Right (x, xs)
-        _ -> errMsg "missing: expected digit, " ns)
+whitespaceP :: Parser ()
+whitespaceP = void $ many spaceP
 
-    boolP :: Parser Bool
-    boolP = (keywordP "true" >> return True) <|> (keywordP "false" >> return False)
+whitespaceRP :: Parser ()
+whitespaceRP = void $ some spaceP
 
-    alphaP :: Parser Char
-    alphaP = Parser $ \ns -> case ns of
-        (x:xs) | isAlpha x -> Right (x, xs)
-        _ -> errMsg "missing: expected alphanumeric, " ns
+numberP :: Parser Int
+numberP =
+  fmap
+    read
+    ( some $ Parser $ \ns -> case ns of
+        (x : xs) | isDigit x -> Just (x, xs)
+        _ -> Nothing
+    )
 
-    alphaNumP :: Parser Char
-    alphaNumP = Parser $ \ns -> case ns of
-        (x:xs) | isAlphaNum x -> Right (x, xs)
-        _ -> errMsg "" ns
+boolP :: Parser Bool
+boolP = (keywordP "true" >> return True) <|> (keywordP "false" >> return False)
 
-    identifierP :: Parser String
-    identifierP = do
-        firstChar <- alphaP
-        remChars <- many alphaNumP
-        return (firstChar:remChars)
+alphaP :: Parser Char
+alphaP = Parser $ \ns -> case ns of
+  (x : xs) | isAlpha x -> Just (x, xs)
+  _ -> Nothing
 
-    newline :: Parser Char
-    newline = characterP '\n'
+alphaNumP :: Parser Char
+alphaNumP = Parser $ \ns -> case ns of
+  (x : xs) | isAlphaNum x -> Just (x, xs)
+  _ -> Nothing
+
+identifierP :: Parser String
+identifierP = do
+  firstChar <- alphaP
+  remChars <- many alphaNumP
+  return (firstChar : remChars)
+
+newline :: Parser Char
+newline = characterP '\n'
