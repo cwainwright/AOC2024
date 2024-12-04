688cf829adc6f6fc8595ecc171017c457f49950c
cwainwright <86470077+cwainwright@users.noreply.github.com>
2024-12-04 06:25:16 +0000

    Update Previous Solutions to use Maybe Parser - Update Formatting

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,42 +1,44 @@
 module Day1 where
-  import Parser(Parser(..), numberP, whitespaceP, parserSeparated)
-  import Data.List (sort)
-  
-  main :: IO()
-  main = do
-    -- Part 1
-    file_contents <- readFile "data/D1.txt"
-    let parsedData = parseData file_contents
-    let (leftList, rightList) = unzip parsedData
-    let (sLeftList, sRightList) = (sort leftList, sort rightList)
-    let sortedLists = zip sLeftList sRightList
-    let differences = map (\(x, y) -> abs (x - y)) sortedLists
-    let total1 = sum differences
-    putStrLn $ "Part 1 Result: " ++ show total1
-    -- Part 1 Complete
-    -- Part 2
-    let similarityScores = map (calculateSimilarity rightList) leftList
-    let total2 = sum similarityScores
-    putStrLn $ "Part 2 Result: " ++ show total2
-    -- Part 2 Complete
 
-  -- Part 1 Helper Functions
-  parseData :: String -> [(Int, Int)]
-  parseData str = 
-    case parse dataParser str of
-      Left _ -> []
-      Right parserData -> (\(finalData, _) -> finalData) parserData
-  
-  dataParser :: Parser [(Int, Int)]
-  dataParser = parserSeparated whitespaceP lineParser
+import Data.List (sort)
+import Parser (Parser (..), numberP, parserSeparated, whitespaceP)
 
-  lineParser :: Parser (Int, Int)
-  lineParser = do
-    l1 <- numberP
-    _ <- whitespaceP
-    l2 <- numberP
-    return (l1, l2)
+main :: IO ()
+main = do
+  -- Part 1
+  file_contents <- readFile "data/D1.txt"
+  let parsedData = parseData file_contents
+  let (leftList, rightList) = unzip parsedData
+  let (sLeftList, sRightList) = (sort leftList, sort rightList)
+  let sortedLists = zip sLeftList sRightList
+  let differences = map (\(x, y) -> abs (x - y)) sortedLists
+  let total1 = sum differences
+  putStrLn $ "Part 1 Result: " ++ show total1
+  -- Part 1 Complete
+  -- Part 2
+  let similarityScores = map (calculateSimilarity rightList) leftList
+  let total2 = sum similarityScores
+  putStrLn $ "Part 2 Result: " ++ show total2
 
-  -- Part 2 Helper Functions
-  calculateSimilarity :: [Int] -> Int -> Int
-  calculateSimilarity rightList number = number * (length $ filter (\x -> x == number) rightList)
+-- Part 2 Complete
+
+-- Part 1 Helper Functions
+parseData :: String -> [(Int, Int)]
+parseData str =
+  case parse dataParser str of
+    Nothing -> []
+    Just parserData -> (\(finalData, _) -> finalData) parserData
+
+dataParser :: Parser [(Int, Int)]
+dataParser = parserSeparated whitespaceP lineParser
+
+lineParser :: Parser (Int, Int)
+lineParser = do
+  l1 <- numberP
+  _ <- whitespaceP
+  l2 <- numberP
+  return (l1, l2)
+
+-- Part 2 Helper Functions
+calculateSimilarity :: [Int] -> Int -> Int
+calculateSimilarity rightList number = number * (length $ filter (\x -> x == number) rightList)
--- src/Day2.hs
+++ src/Day2.hs
@@ -1,53 +1,57 @@
-module Day2(main) where
-    type Safe = Bool
-    data Direction = Up | Down deriving (Eq, Show)
+module Day2 (main) where
 
-    main :: IO ()
-    main = do
-        -- Load File, Parse Contents
-        file_contents <- readFile "data/D2.txt"
-        let file_lines :: [[Int]] = map (\l -> map read (words l)) $ lines file_contents
+type Safe = Bool
 
-        -- Part One
-        let part_1_safe_lines = filter problem file_lines
-        print $ length part_1_safe_lines
+data Direction = Up | Down deriving (Eq, Show)
 
-        -- Part Two
-        let part_2_safe_lines = filter problem_dampner file_lines
-        print $ length part_2_safe_lines
+main :: IO ()
+main = do
+  -- Load File, Parse Contents
+  file_contents <- readFile "data/D2.txt"
+  let file_lines :: [[Int]] = map (\l -> map read (words l)) $ lines file_contents
 
-    line_pairs :: [Int] -> [(Int, Int)]
-    line_pairs line = zip line $ tail line
+  -- Part One
+  let part_1_safe_lines = filter problem file_lines
+  print $ length part_1_safe_lines
 
-    problem :: [Int] -> Safe
-    problem = line_safety.  line_pairs
+  -- Part Two
+  let part_2_safe_lines = filter problem_dampner file_lines
+  print $ length part_2_safe_lines
 
-    line_safety :: [(Int, Int)] -> Safe
-    line_safety pairs = let 
-        safe_direction_pairs = map (\(a, b) -> (pair_differences a b, pair_direction a b)) pairs
-        in 
-        all (\(safety, _) -> safety) safe_direction_pairs -- Check Safe DistancesB
-        && (
-            (all (\(_, direction) -> direction == Up) safe_direction_pairs) -- If all directions are Up
-            || (all (\(_, direction) -> direction == Down) safe_direction_pairs)) -- If all directions are Down
+line_pairs :: [Int] -> [(Int, Int)]
+line_pairs line = zip line $ tail line
 
-    pair_differences :: Int -> Int -> Safe
-    pair_differences a b = let
-             diff = abs (a - b)
-             in diff >= 1 && diff <= 3
+problem :: [Int] -> Safe
+problem = line_safety . line_pairs
 
-    pair_direction :: Int -> Int -> Direction
-    pair_direction a b = if a < b then Up else Down
+line_safety :: [(Int, Int)] -> Safe
+line_safety pairs =
+  let safe_direction_pairs = map (\(a, b) -> (pair_differences a b, pair_direction a b)) pairs
+   in all (\(safety, _) -> safety) safe_direction_pairs -- Check Safe DistancesB
+        && ( (all (\(_, direction) -> direction == Up) safe_direction_pairs) -- If all directions are Up
+               || (all (\(_, direction) -> direction == Down) safe_direction_pairs) -- If all directions are Down
+           )
 
-    line_variations :: [Int] -> [[Int]]
-    line_variations line = map (
-        \i -> let (before, after) = splitAt i line
-            in take (i-1) before ++ after) [1..length line]
+pair_differences :: Int -> Int -> Safe
+pair_differences a b =
+  let diff = abs (a - b)
+   in diff >= 1 && diff <= 3
 
-    problem_dampner :: [Int] -> Safe
-    problem_dampner file_line = do
-        let variations = map line_pairs (line_variations file_line)
-        let variations_safety = zip variations (map line_safety variations)
-        let safe_variations = filter (\(_, safe) -> safe) variations_safety
-        length safe_variations >= 1
-        
+pair_direction :: Int -> Int -> Direction
+pair_direction a b = if a < b then Up else Down
+
+line_variations :: [Int] -> [[Int]]
+line_variations line =
+  map
+    ( \i ->
+        let (before, after) = splitAt i line
+         in take (i - 1) before ++ after
+    )
+    [1 .. length line]
+
+problem_dampner :: [Int] -> Safe
+problem_dampner file_line = do
+  let variations = map line_pairs (line_variations file_line)
+  let variations_safety = zip variations (map line_safety variations)
+  let safe_variations = filter (\(_, safe) -> safe) variations_safety
+  length safe_variations >= 1
