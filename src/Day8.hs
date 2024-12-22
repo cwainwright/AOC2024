9e76119443691611b41c7bc949d844613e170de1
cwainwright <86470077+cwainwright@users.noreply.github.com>
2024-12-22 06:52:48 +0000

    Day 8 - Part Two

--- src/Day8.hs
+++ src/Day8.hs
@@ -14,25 +14,33 @@ main = do
 
   -- Part One
   let antennae = getAntennae grid
-  let groupedAntennae = groupBy (\(_, a) (_, b) -> a == b) (sortBy sortAntennae antennae)
-  let antinodes = concatMap (\(a, b) -> getAntinodes a b) $ concatMap uniquePairs groupedAntennae
-  let validAntinodes = filter (inRange2D grid) antinodes
-  let uniqueAntinodes = map head $ group (sort validAntinodes)
-  print $ length uniqueAntinodes
+      groupedAntennae = groupBy (\(_, a) (_, b) -> a == b) (sortBy sortAntennae antennae)
+      antinodes1 = concatMap getAntinodes $ concatMap uniquePairs groupedAntennae
+      validAntinodes1 = filter (inRange2D grid) antinodes1
+      uniqueAntinodes1 = map head $ group (sort validAntinodes1)
+  print $ length uniqueAntinodes1
+
+  -- Part Two
+  let antinodes2 = concatMap (getAntinodesEx grid) $ concatMap uniquePairs groupedAntennae
+      uniqueAntinodes2 = map head $ group (sort antinodes2)
+  print $ length uniqueAntinodes2
 
 getAntennae :: [[Char]] -> [Antenna]
-getAntennae = filter (\(_, char) -> not (char `elem` ".#")) . enumerate2D
+getAntennae = filter (\(_, char) -> char `notElem` ".#") . enumerate2D
 
 sortAntennae :: Antenna -> Antenna -> Ordering
-sortAntennae (_, a1) (_, a2) =
-  if a1 < a2
-    then LT
-    else
-      if a1 > a2
-        then GT
-        else EQ
+sortAntennae (_, a1) (_, a2)
+  | a1 < a2 = LT
+  | a1 > a2 = GT
+  | otherwise = EQ
 
-getAntinodes :: Antenna -> Antenna -> [Antinode]
-getAntinodes ((ax, ay), _) ((bx, by), _) =
+getAntinodes :: (Antenna, Antenna) -> [Antinode]
+getAntinodes (((ax, ay), _), ((bx, by), _)) =
   let (dx, dy) = (ax - bx, ay - by)
-   in (ax + dx, ay + dy) : (bx - dx, by - dy) : []
+   in [(ax + dx, ay + dy), (bx - dx, by - dy)]
+
+getAntinodesEx :: [[Char]] -> (Antenna, Antenna) -> [Antinode]
+getAntinodesEx grid (((ax, ay), _), ((bx, by), _)) =
+  let (dx, dy) = (ax - bx, ay - by)
+   in takeWhile (inRange2D grid) [(ax + dx * mul, ay + dy * mul) | mul <- [0 ..]]
+        ++ takeWhile (inRange2D grid) [(bx - dx * mul, by - dy * mul) | mul <- [0 ..]]
