5bda476abd6dcfbc5a1db6448b21fb3fa03c33f5
cwainwright <86470077+cwainwright@users.noreply.github.com>
2024-12-22 03:58:38 +0000

    Day 7 - Part 2

--- src/Day7.hs
+++ src/Day7.hs
@@ -2,22 +2,31 @@ module Day7 where
 
 import Control.Applicative (some)
 import Parser (Parser (parse), characterP, numberP, whitespaceP)
+import Prelude hiding ((||))
 
 type YXS = (Int, [Int])
+
+(||) :: Int -> Int -> Int
+(||) a b = read $ (show a) ++ (show b)
 
 main :: IO ()
 main = do
   file_contents <- readFile "data/D7.txt"
   let file_lines = lines file_contents
 
-  -- Part One
-  let operators = [(+), (*)]
   let maybexyss = sequence $ map (fmap fst) $ map (parse parseAnswerValues) file_lines
   case maybexyss of
     Nothing -> print "Nothing"
-    Just xyss ->
-      let vxyss = filter (attemptSolve operators) xyss
-       in print $ sum $ map fst vxyss
+    Just xyss -> do
+      -- Part One
+      let operators1 = [(+), (*)]
+          vxyss1 = filter (attemptSolve operators1) xyss
+       in print $ sum $ map fst vxyss1
+
+      -- Part Two
+      let operators2 = [(+), (*), (||)]
+          vxyss2 = filter (attemptSolve operators2) xyss
+       in print $ sum $ map fst vxyss2
 
 parseAnswerValues :: Parser YXS
 parseAnswerValues = do
